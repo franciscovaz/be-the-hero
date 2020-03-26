00fa29018ab1bc31a9b5eecf4d745a3079b37f01
Francisco Vaz <franciscovaz@MacBook-Pro-de-Francisco.local>
2020-03-26 00:24:27 +0000

    frontend da app

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const ongId = localStorage.getItem('ongId');
+  const history = useHistory();
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value
+    };
+
+    try {
+      const response = await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      alert(`Caso com o ID: ${response.data.id} criado com sucesso.`);
+
+      history.push('/profile');
+
+    } catch (err) {
+      alert('Erro ao registar caso, tente novamente.');
+    }
+  }
+
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero"/>
+
+          <h1>Registar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+          
+          <Link className="back-link"to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form onSubmit={handleNewIncident}>
+          <input 
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Título do caso" />
+
+          <textarea 
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            placeholder="Descrição" />
+
+          <input 
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Valor em euros"/>
+
+          <button className="button" type="submit">Registar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
